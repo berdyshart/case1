8e14de303b3ea5c10d9ef85ac32bb59260557b0b
aszubareva <a24zubareva@gmail.com>
2026-09-06 00:10:05 +0700

    Update flesch_calculators.py

--- infrastructure/flesch_calculators.py
+++ infrastructure/flesch_calculators.py
@@ -1,56 +1,43 @@
-from domain.types import TextStats, Language
+from domain.types import Text_Stats, Language
 
 
-def flesсh_index(stats: TextStats, lang: Language) -> float:
-    """
-    Function to calculate the Flesch index
-    :param stats: text statistics (sentences, words, syllables, averages)
-    :param lang: text language (EN, RU, DE, FR)
-    :return: number (Flesch index)
-    """
+def flesch_index(stats: Text_Stats, lang: Language) -> float:
+  # Рассчитывает индекс удобочитаемости Флеша для текста на указанном языке.
+  if stats.wordCount == 0 and stats.sentenceCount == 0:
+    return 0.0
 
-    if stats.word_count == 0 and stats.sentence_count == 0:
-        return 0.0
+  if lang == Language.EN:
+    return 206.835 - 1.015 * stats.avgSentenceLength - 84.6 * stats.avgWordSyllables
 
-    if lang == Language.EN:
-        return 206.835 - 1.015 * stats.avg_sentence_length - 84.6 * stats.avg_word_syllables
+  elif lang == Language.RU:
+    return 206.835 - 1.3 * stats.avgSentenceLength - 60.1 * stats.avgWordSyllables
 
-    elif lang == Language.RU:
-        return 206.835 - 1.3 * stats.avg_sentence_length - 60.1 * stats.avg_word_syllables
+  elif lang == Language.DE:
+    return 180.0 - 1.0 * stats.avgSentenceLength - 58.5 * stats.avgWordSyllables
 
-    elif lang == Language.DE:
-        return 180.0 - 1.0 * stats.avg_sentence_length - 58.5 * stats.avg_word_syllables
-
-    elif lang == Language.FR:
-        return 207.0 - 1.015 * stats.avg_sentence_length - 73.6 * stats.avg_word_syllables
+  elif lang == Language.FR:
+    return 207.0 - 1.015 * stats.avgSentenceLength - 73.6 * stats.avgWordSyllables
 
 
 def interpret_flesch(score: float, lang: Language) -> str:
-    """
-    Converts Flesch index score into a text interpretation of readability level.
-    :param score: Flesch index value
-    :param lang: text language
-    :return: readability level description in English
-    """
+  # Преобразует значение индекса Флеша в текстовую интерпретацию уровня читаемости.
+  if score >= 90:
+    return 'Very easy'
 
-    if score >= 90:
-        return 'Very easy'
+  elif score >= 80:
+    return 'Easy'
 
-    elif score >= 80:
-        return 'Easy'
+  elif score >= 70:
+    return 'Fairly easy'
 
-    elif score >= 70:
-        return 'Fairly easy'
+  elif score >= 60:
+    return 'Standard'
 
-    elif score >= 60:
-        return 'Standard'
+  elif score >= 50:
+    return 'Fairly difficult'
 
-    elif score >= 50:
-        return 'Fairly difficult'
+  elif score >= 30:
+    return 'Difficult'
 
-    elif score >= 30:
-        return 'Difficult'
-
-    else:
-        return 'Very difficult'
-
+  else:
+    return 'Very difficult'

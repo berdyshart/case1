3c1d0074105c394ce460b928f0dbc03c58caaff5
berdy <berdysevartem96@gmail.com>
2026-09-06 15:59:00 +0700

    changed languages

--- infrastructure/language_detector.py
+++ infrastructure/language_detector.py
@@ -1,9 +1,19 @@
 import fasttext
+import os
+import domain.types
 
 
 TEST1 = "Hello, world! It's a beautiful day, isn't it? (And context-free)."
 TEST2 = "Algoriphobia"
 TEST3 = "Привет, мир! Это прекрасный день, не так ли? Робот-пылесос — круто."
+
+
+LANG_CODE_MAP = {
+    "en": domain.types.Language.EN,
+    "ru": domain.types.Language.RU,
+    "de": domain.types.Language.DE,
+    "fr": domain.types.Language.FR,
+}
 
 
 def detectLanguage(text: str) -> tuple:
@@ -12,9 +22,10 @@ def detectLanguage(text: str) -> tuple:
     :param text: text to detect.
     :return: detected language.
     """
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    MODEL_PATH = os.path.join(BASE_DIR, "..", "lid.176.ftz")
 
-    model_path = "lid.176.ftz"
-    model = fasttext.load_model(model_path)
+    model = fasttext.load_model(os.path.normpath(MODEL_PATH))
     clean_text = text.replace("\n", " ").strip()
 
     predictions = model.predict(clean_text, k=1)
@@ -22,4 +33,6 @@ def detectLanguage(text: str) -> tuple:
     confidence = predictions[1][0]
     lang_code = label.replace("__label__", "")
 
-    return lang_code, confidence
+    lang = LANG_CODE_MAP[lang_code]
+
+    return lang, confidence
--- infrastructure/syllable_counters.py
+++ infrastructure/syllable_counters.py
@@ -10,90 +10,90 @@ TEST3 = "Привет, мир! Это прекрасный день, не так
 
 
 def countSyllablesEnWordSimple(word: str) -> int:
-    """
-    Function to count the number of syllables in a word heuristically.
-    :param word: word to count syllables in.
-    :return: number of syllables.
-    """
-    word = word.lower().strip(".:;?!,()\"'-")
-    if not word:
-        return 0
+  """
+  Function to count the number of syllables in a word heuristically.
+  :param word: word to count syllables in.
+  :return: number of syllables.
+  """
+  word = word.lower().strip(".:;?!,()\"'-")
+  if not word:
+    return 0
 
-    vowels = "aeiouy"
-    count = 0
-    is_prev_vowel = False
+  vowels = "aeiouy"
+  count = 0
+  is_prev_vowel = False
 
-    for char in word:
-        if char in vowels:
-            if not is_prev_vowel:
-                count += 1
-                is_prev_vowel = True
-        else:
-            is_prev_vowel = False
-
-    if word.endswith("e"):
-        count -= 1
-
-    if word.endswith("le") and len(word) > 2 and word[-3] not in vowels:
+  for char in word:
+    if char in vowels:
+      if not is_prev_vowel:
         count += 1
+        is_prev_vowel = True
+    else:
+      is_prev_vowel = False
 
-    if count <= 0:
-        count = 1
+  if word.endswith("e"):
+    count -= 1
 
-    return count
+  if word.endswith("le") and len(word) > 2 and word[-3] not in vowels:
+    count += 1
+
+  if count <= 0:
+    count = 1
+
+  return count
 
 
 def countSyllablesEnWord(word: str) -> int:
-    """
-    Function to count the number of syllables in an english word using nltk.
-    :param word: word to count syllables in.
-    :return: number of syllables.
-    """
-    d = cmudict.dict()
-    word = word.lower()
+  """
+  Function to count the number of syllables in an english word using nltk.
+  :param word: word to count syllables in.
+  :return: number of syllables.
+  """
+  d = cmudict.dict()
+  word = word.lower()
 
-    if word in d:
-        return len([ph for ph in d[word][0] if ph[-1].isdigit()])
-    else:
-        return countSyllablesEnWordSimple(word)
+  if word in d:
+    return len([ph for ph in d[word][0] if ph[-1].isdigit()])
+  else:
+    return countSyllablesEnWordSimple(word)
 
 
 def countSyllablesEn(text: str) -> list:
-    """
-    Function to count the number of syllables in an english text.
-    :param text: english text.
-    :return: number of syllables.
-    """
-    text = text.lower()
-    to_remove = (string.punctuation + "«»—…“”").replace("'", "")
+  """
+  Function to count the number of syllables in an english text.
+  :param text: english text.
+  :return: number of syllables.
+  """
+  text = text.lower()
+  to_remove = (string.punctuation + "«»—…“”").replace("'", "")
 
-    for char in to_remove:
-        text = text.replace(char, " ")
+  for char in to_remove:
+    text = text.replace(char, " ")
 
-    return [countSyllablesEnWord(word) for word in text.split()]
+  return [countSyllablesEnWord(word) for word in text.split()]
 
 
 def countSyllablesRuWord(word: str) -> int:
-    """
-    Function to count the number of syllables in a russian word.
-    :param word: word to count syllables in.
-    :return: number of syllables.
-    """
-    vowels = "аеёиоуыэюя"
-    word = word.lower()
+  """
+  Function to count the number of syllables in a russian word.
+  :param word: word to count syllables in.
+  :return: number of syllables.
+  """
+  vowels = "аеёиоуыэюя"
+  word = word.lower()
 
-    return sum(1 for char in word if char in vowels)
+  return sum(1 for char in word if char in vowels)
 
 
 def countSyllablesRu(text: str) -> list:
-    """
-    Function to count the number of syllables in a russian text.
-    :param text: text to count syllables in.
-    :return:
-    """
-    text = text.lower()
-    to_remove = (string.punctuation + "«»—…“”").replace("-", "")
-    for char in to_remove:
-        text = text.replace(char, " ")
+  """
+  Function to count the number of syllables in a russian text.
+  :param text: text to count syllables in.
+  :return:
+  """
+  text = text.lower()
+  to_remove = (string.punctuation + "«»—…“”").replace("-", "")
+  for char in to_remove:
+    text = text.replace(char, " ")
 
-    return [countSyllablesRuWord(word) for word in text.split()]
+  return [countSyllablesRuWord(word) for word in text.split()]

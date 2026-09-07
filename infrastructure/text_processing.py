caeddb4de822cad9e0b0c3781ed706bf787346c1
Maria Chepeleva <mchepelki@bk.ru>
2026-09-08 00:55:53 +0700

    feat: add isEmptyText function

--- infrastructure/text_processing.py
+++ infrastructure/text_processing.py
@@ -1,6 +1,13 @@
+import string
+
 def isEmptyText(text: str) -> bool:
   """
   Function to check if the incoming text is entirely empty
   """
   return not text or not text.strip()
 
+def isEmptyWord(word: str) -> bool:
+    """
+    Function to check if a specific word is empty
+    """
+    return not word or not word.strip(string.punctuation + "«»—…“”")
